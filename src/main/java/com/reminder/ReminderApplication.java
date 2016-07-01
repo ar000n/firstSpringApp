7611396ab9542e35b309ed351e8a5cbaf24ca00b
Arun <arun@in-arunsathin.local>
2016-07-01 13:27:05 +0530

    done with hello world

---
+++ src/main/java/com/reminder/ReminderApplication.java
@@ -0,0 +1,12 @@
+package com.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderApplication {
+
+	public static void main(String[] args)throws Throwable {
+		SpringApplication.run(ReminderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/reminder/ReminderApplicationTests.java
@@ -0,0 +1,17 @@
+//package com.reminder;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = ReminderApplication.class)
+//@WebAppConfiguration
+//public class ReminderApplicationTests {
+//
+////	@Test
+////	public void contextLoads() {
+////	}
+//
+//}
